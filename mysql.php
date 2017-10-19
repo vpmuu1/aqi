145a3f4378a0a9a982781c4be6f9965b70e0e80e
vpmuu1 <2580336@qq.com>
2017-10-19 10:46:08 +0800

    new file:   health.php 	modified:   mysql.php

--- mysql.php
+++ mysql.php
@@ -1,6 +1,10 @@
 <?php
-$mysql_host = "172.30.180.109";
+$servername = getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_HOST");
+$username = getenv("DATABASE_USER");
+$password = getenv("DATABASE_PASSWORD");
+
+$mysql_host = $servername;
 $mysql_database = "default";
-$mysql_user = "cakephp";
-$mysql_password = "WwamxpfastGiDq5D";
+$mysql_user = $username;
+$mysql_password = $password;
 ?>
